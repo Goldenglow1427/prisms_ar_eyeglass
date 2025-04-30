d9f52b0fbc97ed26884df50292b560f5737525c5
Goldenglow1427 <magician1427@gmail.com>
2025-04-30 10:34:01 -0400

    Syncronize progress.

--- src/oled/lib/waveshare_OLED/config.py
+++ src/oled/lib/waveshare_OLED/config.py
@@ -71,7 +71,9 @@ def module_init():
     # GPIO.setmode(GPIO.BCM)
     # GPIO.setwarnings(False)
     # GPIO.setup(RST_PIN, GPIO.OUT)
-    # rst.close()
+
+    if rst is not None:
+        rst.close()
     rst = LED(RST_PIN)
 
     # GPIO.setup(DC_PIN, GPIO.OUT)
